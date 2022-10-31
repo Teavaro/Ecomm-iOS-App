dc3d795cd969a7f3e4dbb8bdf326acb2958e875b
iranvibo <vicenteiran@gmail.com>
2022-10-31 11:33:54 -0400

    add missing files

---
+++ Teavaro Ecomm Demo App/Teavaro Ecomm Demo App/UserDefaultsUtils.swift
@@ -0,0 +1,53 @@
+//
+//  UserDefaultsUtils.swift
+//  iosApp
+//
+//  Created by Ahmad Mahmoud on 09/03/2022.
+//
+
+import Foundation
+
+class UserDefaultsUtils {
+    
+    private static let CDP_CONSENT = "CDP_CONSENT"
+    private static let CDP_OM = "CDP_OM"
+    private static let CDP_OPT = "CDP_OPT"
+    private static let CDP_NBA = "CDP_NBA"
+    private static let userDefaults = UserDefaults.standard
+    
+    static func isCdpConsentAccepted() -> Bool {
+        return self.userDefaults.bool(forKey: CDP_CONSENT)
+    }
+    
+    static func acceptCdpConsent() {
+        self.userDefaults.set(true, forKey: CDP_CONSENT)
+    }
+    
+    static func rejectCdpConsent() {
+        self.userDefaults.set(false, forKey: CDP_CONSENT)
+    }
+    
+    static func isCdpOm() -> Bool {
+        return self.userDefaults.bool(forKey: CDP_OM)
+    }
+    
+    static func setCdpOm(om: Bool) {
+        self.userDefaults.set(om, forKey: CDP_OM)
+    }
+    
+    static func isCdpOpt() -> Bool {
+        return self.userDefaults.bool(forKey: CDP_OPT)
+    }
+    
+    static func setCdpOpt(opt: Bool) {
+        self.userDefaults.set(opt, forKey: CDP_OPT)
+    }
+    
+    static func isCdpNba() -> Bool {
+        return self.userDefaults.bool(forKey: CDP_NBA)
+    }
+    
+    static func setCdpNba(nba: Bool) {
+        self.userDefaults.set(nba, forKey: CDP_NBA)
+    }
+}
